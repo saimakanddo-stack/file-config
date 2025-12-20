e81b0a0019256c8f5d8b38755557651602c0c226
saimakanddo-stack <saimakanddo@gmail.com>
2025-12-20 14:56:31 +0600

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,12 @@
+{
+  "success": true,
+  "data": {
+    "id": "global",
+    "ads_limit": 20,
+    "tutorial_youtube_id": null,
+    "channels": [],
+    "filters": [],
+    "created_at": "2025-12-03 09:08:31",
+    "updated_at": "2025-12-03 09:08:31"
+  }
+}
