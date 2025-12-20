61ae3b596010293f11fac25e2337baba698fc41f
saimakanddo-stack <saimakanddo@gmail.com>
2025-12-20 14:54:47 +0600

    Update files.php

--- files.php
+++ files.php
@@ -1,1 +1,114 @@
 
+{
+  "success": true,
+  "data": [
+    {
+      "id": 14,
+      "name": "পুরাই নতুন",
+      "description": "",
+      "image_url": "https://i.ibb.co/jPx9hvwQ/0327087b1279.jpg",
+      "download_link": "https://qu.ax/rTSNW",
+      "ad_watches_required": 5,
+      "category": "",
+      "daily_limit": null,
+      "created_at": "2025-12-10 19:43:27",
+      "updated_at": "2025-12-10 19:43:27"
+    },
+    {
+      "id": 13,
+      "name": "দেখতে কেমন লাগে মামা",
+      "description": "",
+      "image_url": "https://i.ibb.co/QFvRQNX9/f0e30521d7d6.jpg",
+      "download_link": "https://pomf2.lain.la/f/qaezrxdb.mp4",
+      "ad_watches_required": 5,
+      "category": "",
+      "daily_limit": null,
+      "created_at": "2025-12-09 16:35:02",
+      "updated_at": "2025-12-09 16:35:02"
+    },
+    {
+      "id": 12,
+      "name": "Part 2",
+      "description": "",
+      "image_url": "https://i.ibb.co/h1ZF8KqS/773ffdba69c2.jpg",
+      "download_link": "https://pomf2.lain.la/f/iv0zs225.mp4",
+      "ad_watches_required": 5,
+      "category": "",
+      "daily_limit": null,
+      "created_at": "2025-12-08 18:52:56",
+      "updated_at": "2025-12-08 18:52:56"
+    },
+    {
+      "id": 11,
+      "name": "Part 1",
+      "description": "",
+      "image_url": "https://i.ibb.co/h1ZF8KqS/773ffdba69c2.jpg",
+      "download_link": "https://pomf2.lain.la/f/5i0ygrjd.mp4",
+      "ad_watches_required": 5,
+      "category": "",
+      "daily_limit": null,
+      "created_at": "2025-12-08 18:52:35",
+      "updated_at": "2025-12-08 18:52:35"
+    },
+    {
+      "id": 10,
+      "name": "দেখতে নতুন নাকি মামা",
+      "description": "",
+      "image_url": "https://i.ibb.co/7dRgQ0WS/fc69d07b7783.jpg",
+      "download_link": "https://pomf2.lain.la/f/f0hvtvxl.mp4",
+      "ad_watches_required": 5,
+      "category": "",
+      "daily_limit": null,
+      "created_at": "2025-12-07 19:32:32",
+      "updated_at": "2025-12-07 19:32:32"
+    },
+    {
+      "id": 9,
+      "name": "New TikToker Video",
+      "description": "",
+      "image_url": "https://i.postimg.cc/xCTNk6Js/image.png",
+      "download_link": "https://qu.ax/nhRYa",
+      "ad_watches_required": 5,
+      "category": "",
+      "daily_limit": null,
+      "created_at": "2025-12-06 08:24:03",
+      "updated_at": "2025-12-06 08:24:03"
+    },
+    {
+      "id": 8,
+      "name": "শফিক ভাই পুরাই আগুন",
+      "description": "",
+      "image_url": "https://i.ibb.co/4ZGqLN5K/a19eda8bfb11.jpg",
+      "download_link": "https://qu.ax/sumei.mp4",
+      "ad_watches_required": 5,
+      "category": "",
+      "daily_limit": null,
+      "created_at": "2025-12-05 20:36:12",
+      "updated_at": "2025-12-05 20:36:58"
+    },
+    {
+      "id": 7,
+      "name": "Unlock করে দেখুন",
+      "description": "",
+      "image_url": "https://i.postimg.cc/KjbGkzHG/image.png",
+      "download_link": "https://pomf2.lain.la/f/biksjtbu.mp4",
+      "ad_watches_required": 5,
+      "category": "",
+      "daily_limit": null,
+      "created_at": "2025-12-04 17:38:48",
+      "updated_at": "2025-12-04 17:38:48"
+    },
+    {
+      "id": 6,
+      "name": "Unlock এ ক্লিক করে এড দেখে আনলক করুন",
+      "description": "",
+      "image_url": "https://i.ibb.co/gZtJkX5y/ecfe56edf552.jpg",
+      "download_link": "https://qu.ax/GLlhL",
+      "ad_watches_required": 5,
+      "category": "",
+      "daily_limit": null,
+      "created_at": "2025-12-03 18:53:19",
+      "updated_at": "2025-12-03 18:53:19"
+    }
+  ]
+}
